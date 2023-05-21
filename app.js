0d28459a64c6bbd4d3d389d72b5789d4c108c4ea
Benny <wonsang222@naver.com>
2023-05-21 14:45:13 +0900

    얼추 써버 구축 완료

--- app.js
+++ app.js
@@ -8,11 +8,17 @@ const cookieParser = require('cookie-parser');
 const indexRouter = require('./routes/index');
 const guessWhoRouter = require('./routes/guessWho');
 const {sequelize} = require('./models');
+const fs = require('fs');
 
 const app = express();
 app.set('port', process.env.PORT || 8001);
 app.use(morgan('dev'));
 app.use(express.json());
+
+const filePath = path.join(__dirname, 'tempDB', 'CharDB.json');
+const fileData = fs.readFileSync(filePath);
+const guessWhoDB = JSON.parse(fileData);
+
 
 sequelize.sync({force:false})
 .then(() => {
@@ -22,26 +28,44 @@ sequelize.sync({force:false})
 })
 
 // 버전, 아이폰, 
-app.use()
+app.use((req, res, next) =>{
+  console.log('일단 미들웨어 진입');
+  const userAgent = req.header('User-Agent');
+  const uuid = req.header('Authorization');
+  console.log('uuid' + uuid);
+  console.log('useragent' + userAgent);
 
-app.get('./guessWho/:howmany', (req, res) => {
-  let {howmany} = req.params;
-  // 여기서 짜르기
+  if (userAgent == process.env.BUNDLE && uuid == process.env.UUID){
+    console.log('여기타니');
+    next('route');
+  } else{
+    const err = new Error('잘못된 접근입니다.');
+    console.log('여기타니2');
+    err.status = 404;
+    next(err);
+  }
+})
 
+app.get('/guessWho', (req, res, next) => {
+  console.log('guessWhoGET')
+  res.status = 200;
+  res.json(guessWhoDB);
 })
 
 app.use((req, res, next) => {
+  console.log('에러미들웨어');
   const error = new Error('라우터가 없습니다.');
   error.status = 404;
   next(error);
 });
 
 app.use((err, req, res, next) => {
+  console.log('마지막에러');
   res.locals.message = err.message;
   res.locals.error = process.env.NODE_ENV !== 'production' ? err:{};
   res.status(err.status || 500);
+  
 });
-
 
 app.listen(app.get('port'), () => {
   console.log(app.get('port'), '번 포트에서 대기중');
--- routes/guessWho.js
+++ routes/guessWho.js
@@ -1,8 +1,13 @@
 const express = require('express');
 
-const router =express.Router();
+const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res,next) => {
+  console.log('첫번째');
+  
+  // 여기서 짜르기
+  console.log(howmany);
+  
   res.send('hello, express');
 });
 
