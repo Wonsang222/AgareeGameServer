9f7e83527776a929d9c5f6ca46b1d005aabce412
Benny <wonsang222@naver.com>
2023-07-09 17:07:37 +0900

    1차 완성

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ app.use(express.json());
 
 const rateLimit = require('express-rate-limit'); 
 const { MIMEType } = require('util');
+const { error } = require('console');
+const { type } = require('os');
 
 const limiter = rateLimit({
   windowMx: 60 * 1000,
@@ -32,93 +34,87 @@ const limiter = rateLimit({
 const UUID = '59287382-e52d-4090-a829-864b5b578bc1';
 const BUNDLE = 'com.kr.magic';
 
-// ------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 // app.use(rateLimit);
 
 // 버전, 아이폰, 
-app.use((req, res, next) =>{
-  console.log('첫번째');
+
+app.use((req, res, next) => {
   const userAgent = req.header('User-Agent');
   const uuid = req.header('Authorization');
-  
-  if (userAgent == BUNDLE && uuid == UUID){
-    const howMany = req.query.num;
-    res.locals.howMany = howMany;
+
+  if (userAgent == BUNDLE && uuid == UUID) {
+    res.locals.howMany = req.query.numberOfPlayers;
     next('route');
-  } else{
+  } else {
     const newerr = new Error('비정상 접근');
-    newerr.statuscode = 404;
+    newerr.statusCode = 404;
     next(newerr);
   }
 });
 
 app.get('/guessWho', (req, res, next) => {
-  console.log('guessWhoGET')
-  res.statuscode = 200;
   const howMany = res.locals.howMany;
-  const number = parseInt(howMany, 10);
-
-  if (number < 1 || number > 5) {
+  const people = parseInt(howMany, 10);
+  if (people > 5) {
     const err = new Error('잘못된 접근입니다.');
-    err.statuscode = 300;
+    err.statusCode = 300;
     next(err);
     return;
   }
 
-  const result = getRandomProperties(howMany);
-  
+  const result = getRandomProperties(people);
   res.send(result);
 
-  function getRandomProperties(count){
+  function getRandomProperties(count) {
     const randomObj = {};
     const selectedIndex = new Set();
-  
-    while (selectedIndex.size < count){
+    let idx = 0;
+
+    while (selectedIndex.size < count) {
       const randomIdx = Math.floor(Math.random() * imageFiles.length);
-      if (selectedIndex.has(randomIdx)){
+      if (selectedIndex.has(randomIdx)) {
         continue;
       }
       selectedIndex.add(randomIdx);
-    
+
       const name = imageFiles[randomIdx].replace(/\.(png|jpg)$/i, '');
-      randomObj[name] = `http://localhost:8080/guessWho/${encodeURIComponent(imageFiles[randomIdx])}`;
+      randomObj[idx] = `http://localhost:8080/photos/${encodeURIComponent(name)}`;
+      idx += 1;
     }
-    return randomObj
-  } 
+    return randomObj;
+  }
 });
 
-app.use('/guessWho/:filename', (req, res) => {
-  console.log('두번째');
-  const fileName = req.params.filename;
-  const userAgent = req.header('User-Agent');
-  const uuid = req.header('Authorization');
+app.use('/photos/:filename', (req, res) => {
+  const filePath = path.join(__dirname, 'tempDB', 'photoIndex.json');
   
-  if (userAgent == BUNDLE && uuid == UUID){
-    let jpgPath = path.join(__dirname, 'resources', 'GuessWho', `${fileName}.jpg`);
-    let pngPath = path.join(__dirname, 'resources', 'GuessWho', `${fileName}.png`);
-
-    fs.access(jpgPath, fs.constants.F_OK, (err) => {
-      if (!err){
-        res.sendFile(jpgPath);
-      } else {
-        fs.access(pngPath, fs.constants.F_OK, (err) => {
-          if (!err){
-            res.sendFile(pngPath);
-          } else {
-            const newErr = new Error('비정상접근');
-            newerr.statuscode = 404;
-            next(newErr);
-          }
-        });
-      }
-    });
-  } else{
-    const newerr = new Error('비정상 접근');
-    newerr.statuscode = 404;
-    next(newerr);
-  }
-})
+  fs.readFile(filePath, (err, data) => {
+    if (err){
+      const newerr = new Error('비정상 접근');
+      newerr.statuscode = 404;
+      next(newerr);
+      return;
+    }
+    try{
+      const fileName = req.params.filename;
+      const idx = parseInt(fileName, 10);
+      console.log(idx);
+      const index = JSON.parse(data);
+      const targetName = encodeURIComponent(index[idx]);
+      
+      let jpgPath = path.join(__dirname, 'resources', 'GuessWho', `${fileName}.jpg`);
+      res.statusCode = 200;
+      res.setHeader('target-name', targetName);
+      res.sendFile(jpgPath);
+    } catch (error){
+      const newerr = new Error('비정상 접근');
+      newerr.statuscode = 404;
+      return next(newerr);
+    }
+  });
+});
 
 app.use((req, res, next) => {
   console.log('에러미들웨어');
@@ -128,7 +124,8 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  res.status(err.statuscode || 500).json({});
+  res.status(err.status || 500);
+  res.send({ error: err.message });
 });
 
 app.listen(app.get('port'), '0.0.0.0', () => {
--- saver.js
+++ saver.js
@@ -2,6 +2,38 @@ const fs = require('fs');
 const axios = require('axios');
 const path = require('path');
 
+const folderPath = './resources/GuessWho';
+const fileExtension = '.jpg'; // 파일 확장자 설정
+let map = new Map();
+
+fs.readdir(folderPath, (err, files) => {
+  if (err) {
+    console.error('Error reading folder:', err);
+    return;
+  }
+
+  files.forEach((file, index) => {
+    const oldPath = path.join(folderPath, file);
+    const newPath = path.join(folderPath, `${index}${fileExtension}`);
+
+    console.log(file == '혜인.jpg');
+
+    console.log(file.replace(/\.(png|jpg)$/i, ''));
+    console.log(index);
+    // fs.rename(oldPath, newPath, (err) => {
+    //   if (err) {
+    //     console.error(`Error renaming file ${file}:`, err);
+    //   } else {
+    //     console.log(`Renamed ${file} to ${index}${fileExtension}`);
+    //     map.set(index, )
+        
+    //   }
+    // });
+  });
+});
+
+
+
 // const filePath = path.join(__dirname, 'tempDB', 'CharDB.json');
 // const fileData = fs.readFileSync(filePath);
 // const guessWhoDB = JSON.parse(fileData);
